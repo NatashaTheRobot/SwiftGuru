552f5884c7a7236ebce7ae300fe9c63acf08ea0a
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2017-11-08 01:48:21 +0530

    New examples

--- 04-collections-sequences.playground/Pages/1.xcplaygroundpage/Contents.swift
+++ 04-collections-sequences.playground/Pages/1.xcplaygroundpage/Contents.swift
@@ -1,120 +1,52 @@
-/*:
- ## Collections and Sequences
-
- This playground will help you build your own collection type. After going through this playground you must be able to appreciate how conforming to the different collection protocols gives you standard library functionality in return, for your own types.
-
- #### The Protocols
-
- The different protocols that we will play around today are:
- 1. Sequence
- 2. Collection
- 3. BidirectionalCollection
- 4. RandomAccessCollection
- 5. MutableCollection
- 6. RangeReplacableCollection
-
- #### Example
-
- Matrices have always been super important in the various disciplines of computer science, right from graph algorithms to deep learning. Deep learning is soon becoming mainstream and so is linear algebra that the former is based upon. Thinking in terms of matrices is common practice in deep learning algorithms!
-
- Let us attempt to implement a simple matrix type, which has an array as its backing data structure. Let us then conform to the various protocols listed above and get a first hand experience of the rich functionality we get in return. We will work with a very simple 2-D matrix abstraction. The dimensions of a matrix are represented by the pair (m, n) where m = number of rows and n = number of columns.
-
- We may want to support the reshaping operation. Given a (m, n) matrix we may want to reshape it to (p, r) under the constraint that m * n == p * r.
-
- So, let us write a simple Matrix type.
- */
 import Foundation
+import XCTest
 
-struct Matrix2D {
+struct Program {
+    let text: String
+    
+    public init(_ text: String) {
+        self.text = text
+    }
+}
+
+extension Program : Sequence {
+    func makeIterator() -> TokenIterator {
+        return TokenIterator(self)
+    }
+}
+
+struct TokenIterator : IteratorProtocol {
+    let tokens: [String]
+    var index = 0
+    
+    init( _ program: Program) {
+        tokens = program.text
+            .components(separatedBy: .whitespacesAndNewlines)
+            .filter { $0.count > 0 }
+    }
+    
+    mutating func next() -> String? {
+        guard index < tokens.count else { return nil }
+        defer { index += 1 }
+        return tokens[index]
+    }
+}
+
+struct Matrix {
     var elements: [Int]
     var rows: Int
     var columns: Int
-
+    
     init(_ elements: [Int], rows: Int, columns: Int) {
         precondition(rows * columns == elements.count)
         self.elements = elements
         self.rows = rows
         self.columns = columns
     }
-
-    mutating func reshape(rows: Int, columns: Int) {
-        precondition(rows * columns == self.rows * self.columns)
-        self.rows = rows
-        self.columns = columns
-    }
 }
 
-/*:
- ##### Sequence: iterating through the rows of the matrix
- As a part of our deep learning package we want to have the ability to iterate through the matrix, one row at a time! How do we implement this functionality without actually looping through the array? The `Sequence` protocol is one possible way.
- Once our type is a Sequence, we can use the `for-in` construct and also it's functional counterpart `forEach`. All we need is to make our type conform to Sequence and create an iterator that conforms to IteratorProtocol.
-
- Please refer to: https://developer.apple.com/documentation/swift/iteratorprotocol
- */
-extension Matrix2D: Sequence {
-    func makeIterator() -> RowIterator {
-        //Exercise begins
-        return RowIterator(self)
-        //Exercise ends
-    }
-}
-
-struct RowIterator: IteratorProtocol {
-    let matrix: Matrix2D
-    var index = 0
-
-    init(_ matrix: Matrix2D) {
-        self.matrix = matrix
-    }
-
-    mutating func next() -> [Int]? {
-        //Exercise begins
-        guard index < matrix.rows else { return nil }
-        defer { index += 1 }
-        return Array(matrix.elements[(index * matrix.columns)..<((index + 1) * matrix.columns)])
-        //Exercise ends
-    }
-}
-
-var matrix = Matrix2D(Array(1...24), rows: 3, columns: 8)
-/*:
- > The for-in loop simply works with our type now.
- */
-for row in matrix {
-    print(row)
-}
-matrix.reshape(rows: 6, columns: 4)
-/*:
- > And this must print the reshaped rows
- */
-matrix.forEach { print($0) }
-
-/*:
- > Printing the sum of every row of the matrix
- */
-let m = matrix.map {
-    row in row.reduce(0,+)
-}
-print(m)
-
-/*:
- > Printing only those rows whose sum is divisible by 10
- */
-//Exercise begins
-matrix.filter { row in row.reduce(0, +) % 10 == 0 }
-    .forEach { print($0) }
-//Exercise ends
-
-/*:
- ##### Collection: making Matrix2D row indexable
-* matrix.first must return the first row
-* matrix[2] must return the second row
-* matrix[2...4] must return rows 2, 3 and 4
-
- Refer to https://developer.apple.com/documentation/swift/collection
- */
-extension Matrix2D: MutableCollection /*Collection first, then mutable*/ {
-
+extension Matrix: MutableCollection {
+    
     var startIndex: Int {
         return 0
     }
@@ -138,49 +70,101 @@ extension Matrix2D: MutableCollection /*Collection first, then mutable*/ {
         }
     }
 }
-/*:
- > Print the first row of the matrix
- */
-print(matrix.first!)
 
-/*:
- > Print the third row of the matrix
- */
-print(matrix[2])
-/*:
- > Print rows 2, 3 and 4 of the matrix
- */
-matrix[2...4].forEach { print($0) }
-
-/*:
- ##### BidirectionalCollection: support backward traversal
- * Find the last row of a matrix
- * Return the last 4 rows of the matrix
- */
-//Exercise begins
-extension Matrix2D: BidirectionalCollection {
+extension Matrix: BidirectionalCollection {
     func index(before idx: Int) -> Int {
         return idx - 1
     }
 }
-//Exercise ends
 
-//: > Print the last row of a matrix
-print(matrix.last!)
+extension Matrix {
+    func first() -> Element? {
+        return self.first
+    }
+    
+    func rows(upTo idx: Int) -> SubSequence? {
+        return self.prefix(upTo: idx)
+    }
+    
+    func row(at idx: Int) -> Element? {
+        return self[idx]
+    }
+    
+    func rows(in range: Range<Int>) -> SubSequence? {
+        return self[range]
+    }
+    
+    func last() -> Element? {
+        return self.last
+    }
+    
+    func rows(from index: Int) -> SubSequence? {
+        return self.suffix(from: index)
+    }
+    
+    mutating func replaceWithZeros(at index: Int) {
+        self[index] = Array(repeatElement(0, count: columns))
+    }
+    
+    mutating func swapRows(at idx: Int, with: Int) {
+        self.swapAt(idx, with)
+    }
+    
+    func sums() -> [Int] {
+        return []
+    }
+    
+    func sums(predicate: ([Int]) -> Bool) -> [Int] {
+        return []
+    }
+    
+}
 
-//: > Remove the last two rows of a given matrix
-matrix.suffix(4).forEach { print($0) }
+//Tests
+//Sequences
+let text = """
+struct Program {
+    let text : String
+    public init ( _ text : String ) {
+        self.text = text
+    }
+}
+"""
 
+let program = Program(text)
+XCTAssertEqual(program.filter { $0 == "}" || $0 == "{" }.count, 4)
 
-/*:
- ##### MutableCollection: modifying the elements of a collection
- * Swapping the rows at two given indices
- * Sorting the collection in place
- */
+//Collections
+var matrix = Matrix(Array(1...24), rows: 6, columns: 4)
+XCTAssertEqual(matrix.first()!, [1, 2, 3, 4])
 
-//>: Swapping rows 0 and 2
-matrix.swapAt(0, 2)
-matrix.forEach { print($0) }
+let rows = matrix.rows(upTo: 2)
+XCTAssertNotNil(rows)
+XCTAssertEqual(rows![0], [1, 2, 3, 4])
+XCTAssertEqual(rows![1], [5, 6, 7, 8])
 
-//>: Sort the rows by the first element
-matrix.sorted { $0[0] > $1[0] }.forEach { print($0) }
+let rowAt2 = matrix.row(at: 2)
+XCTAssertNotNil(rowAt2)
+XCTAssertEqual(rowAt2!, [9, 10, 11, 12])
+
+let rows2to4 = matrix.rows(in: Range<Int>(2...4))
+XCTAssertNotNil(rows2to4)
+
+XCTAssertEqual(rows2to4![2], [9, 10, 11, 12])
+XCTAssertEqual(rows2to4![3], [13, 14, 15, 16])
+XCTAssertEqual(rows2to4![4], [17, 18, 19, 20])
+
+XCTAssertEqual(matrix.last()!, [21, 22, 23, 24])
+
+let lastThreeRows = matrix.rows(from: 3)
+XCTAssertNotNil(lastThreeRows)
+XCTAssertEqual(lastThreeRows![3], [13, 14, 15, 16])
+XCTAssertEqual(lastThreeRows![4], [17, 18, 19, 20])
+XCTAssertEqual(lastThreeRows![5], [21, 22, 23, 24])
+
+matrix.replaceWithZeros(at: 0)
+XCTAssertEqual(matrix.first()!, [0, 0, 0, 0])
+
+matrix.swapRows(at: 0, with: 5)
+XCTAssertEqual(matrix.first()!, [21, 22, 23, 24])
+XCTAssertEqual(matrix.last()!, [0, 0, 0, 0])
