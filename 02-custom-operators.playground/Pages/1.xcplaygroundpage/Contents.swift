3b9c76a027070928643e533ff0cb1e139f5e27b2
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2017-11-09 14:38:57 +0530

    Custom operators - basics

--- 02-custom-operators.playground/Pages/1.xcplaygroundpage/Contents.swift
+++ 02-custom-operators.playground/Pages/1.xcplaygroundpage/Contents.swift
@@ -1,6 +1,37 @@
-//: Playground - noun: a place where people can play
+import Foundation
+import XCTest
+/*:
+####Quicksort
+*/
+func qsort<T: Comparable>(_ array: [T]) -> [T] {
+    guard array.count > 1 else { return array }
+    let pivot = array[0]
+    return qsort(array.filter { $0 < pivot }) + qsort(array.filter { $0 == pivot }) + qsort(array.filter { $0 > pivot })
+}
 
-import UIKit
+/*:
+ #####Quick sort using custom operators
+ 
+ Let us have an operator that returns the first element of an array. Lets call it ⍴
+ */
+prefix operator ⍴
+prefix func ⍴<T>(w: [T]) -> T {
+    return w[0]
+}
 
-var str = "Hello, playground"
+prefix operator ⚀
+prefix func ⚀<T>(w: [T]) -> Int {
+    return w.count
+}
 
+infix operator ¦
+func ¦<T> (w: [T], predicate: (T)->Bool) -> [T] {
+    return w.filter(predicate)
+}
+
+func q<T : Comparable>(_ w: [T]) -> [T] {
+    return ⚀w <= 1 ? w : q(w ¦ { $0 < ⍴w }) + q(w ¦ { $0 == ⍴w }) + q(w ¦ { $0 > ⍴w })
+}
+
+q([10, 9, 8, 6, 4])
+q(["Tokyo", "New York", "Bangalore"])
